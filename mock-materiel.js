d864a139a64ae20c6c7cd5bfcef099e6ae63f1a9
cyyyp10 <115178041+cyyyp10@users.noreply.github.com>
2023-12-14 17:38:53 +0100

    Add files via upload

---
+++ mock-materiel.js
@@ -0,0 +1,12 @@
+const materiels = [
+    {
+     id: 1,
+     name: "Table",
+     quantite_mecessaire: 25,
+     quantite_en_stock : "28",
+     oquantite_sur_place: "30",
+     contact_location: "03772993"
+    }
+   ];
+     
+module.exports = materiels
